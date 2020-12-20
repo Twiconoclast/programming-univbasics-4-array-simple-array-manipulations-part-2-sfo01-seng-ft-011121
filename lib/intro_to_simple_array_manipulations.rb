b0b5fff19dd12b369189d74cc0b0f362fe18d2b2
Carrie Bares <caroltbares@gmail.com>
2020-12-20 02:45:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,3 +10,16 @@ end
 def using_uniq(array)
   return array.uniq
 end
+
+def using_flatten(array)
+  return array.flatten
+end
+
+def using_delete(array, string)
+  return array.delete(string)
+end
+
+def using_delete_at(array, n)
+  return array.delete_at(n)
+end
+
